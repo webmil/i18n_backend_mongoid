fdbc900ebff575227e5e8fd4a6a30bbb5a228152
Rubich Viktor <syzspectroom@gmail.com>
2016-10-27 18:22:52 +0300

    rewritten mongoid adapter

--- lib/generators/templates/mongoid.rb
+++ lib/generators/templates/mongoid.rb
@@ -45,25 +45,44 @@ module I18n
 
         protected
 
-          def init_translations
-            load_translations
-            @initialized = true
-          end
+        def init_translations
+          load_translations
+          @initialized = true
+        end
 
-          def load_translations
-            @translations = {}
-            @model.all.each{ |t| @lookuptranslations[t.key] = t.value_translations }
+        def load_translations
+          @translations = {}
+          @model.all.each do |record|
+            I18n.available_locales.each do |loc|
+              next unless record.value_translations.key?(loc)
+              k = I18n.normalize_keys(loc, record.key, [])
+              k.shift
+              store_translations(loc, k.reverse.inject(record.value_translations[loc]) { |a, n| { n => a } })
+            end
           end
+        end
 
-          def translations
-            @translations ||= {}
-          end
+        def translations
+          @translations ||= {}
+        end
 
-          def lookup(locale, key, scope = [], options = {})
-            init_translations unless initialized?
-            #translations.dig(key,value)
-             translations[key].blank? ? nil : translations[key][locale]
+        # Looks up a translation from the translations hash. Returns nil if
+        # eiher key is nil, or locale, scope or key do not exist as a key in the
+        # nested translations hash. Splits keys or scopes containing dots
+        # into multiple keys, i.e. <tt>currency.format</tt> is regarded the same as
+        # <tt>%w(currency format)</tt>.
+        def lookup(locale, key, scope = [], options = {})
+          init_translations unless initialized?
+          keys = I18n.normalize_keys(locale, key, scope, options[:separator])
+
+          keys.inject(translations) do |result, k|
+            k = k.to_sym
+            return nil unless result.is_a?(Hash) && result.key?(k)
+            result = result.is_a?(Symbol) ? resolve(locale, k, result, options.merge(scope: nil)) : result[k]
+
+            result
           end
+        end
       end
 
       include Implementation
